016813687f04ac98d6bdcc2efe58ed98f95c76dd
akincengiz <akincengiz@outlook.com.tr>
2025-06-10 22:39:10 +0300

    Add project files.

---
+++ src/projects/YmypMovieProject.DataAccess/Contexts/MovieDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using YmypMovieProject.Entity.Entities;
+
+namespace YmypMovieProject.DataAccess.Contexts;
+public sealed class MovieDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=AKINCENGIZ;Initial Catalog=YmypMovieDb;Integrated Security=True;Trust Server Certificate=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Movie>().Property(p => p.IMDB).HasPrecision(4, 2);
+        modelBuilder.Entity<Movie>().Property(p => p.Name).HasMaxLength(100).IsRequired();
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<Actor> Actors { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Director> Directors { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+}
