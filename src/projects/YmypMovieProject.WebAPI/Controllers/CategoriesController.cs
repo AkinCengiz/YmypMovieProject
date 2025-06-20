8e3d9d68330727465e002f701879113de55710ee
akincengiz <akincengiz@outlook.com.tr>
2025-06-20 22:41:12 +0300

    ManuelMaapper completed

--- src/projects/YmypMovieProject.WebAPI/Controllers/CategoriesController.cs
+++ src/projects/YmypMovieProject.WebAPI/Controllers/CategoriesController.cs
@@ -1,18 +1,25 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using YmypMovieProject.Business.Abstract;
+using YmypMovieProject.Business.Mappers.Categories;
+using YmypMovieProject.Entity.Dtos.Categories;
+using YmypMovieProject.Entity.Dtos.Movies;
 using YmypMovieProject.Entity.Entities;
 
 namespace YmypMovieProject.WebAPI.Controllers;
+
 [Route("api/[controller]")]
 [ApiController]
 public class CategoriesController : ControllerBase
 {
     private readonly ICategoryService _categoryService;
+    private readonly ManuelCategoryMapper _mapper;
 
     public CategoriesController(ICategoryService categoryService)
     {
         _categoryService = categoryService;
+        _mapper = new ManuelCategoryMapper(); // Assuming you have a manual mapper for categories
     }
 
     [HttpGet]
@@ -57,5 +64,23 @@ public class CategoriesController : ControllerBase
         return Ok();
     }
 
-}
+    [HttpGet("GetAllFullInfo")]
+    public IActionResult GetAllFullInfo()
+    {
+        var categories = _categoryService.GetQueryable().Include(c => c.Movies).ToList();
+        
+        //List<CategoryResponseDto> dtos = new List<CategoryResponseDto>();
+        //foreach (var category in categories)
+        //{
+        //    dtos.Add(new CategoryResponseDto()
+        //    {
+        //        Id = category.Id,
+        //        Name = category.Name ?? string.Empty,
+        //        Description = category.Description ?? string.Empty
+        //    });
+        //}
 
+        var dtos = _mapper.ConvertToResponseList(categories);
+        return Ok(dtos);
+    }
+}
