4518e6291bc1047f9f09e9c6e4597168f801ee84
akincengiz <akincengiz@outlook.com.tr>
2025-07-15 22:39:00 +0300

    Jwt and token class completed.

--- core/Core.Business/Utilites/Security/Jwt/TokenOptions.cs
+++ core/Core.Business/Utilites/Security/Jwt/TokenOptions.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace Core.Business.Utilites.Security.Jwt;
 public class TokenOptions
 {
-    public string Audience { get; set; }
-    public string Issuer { get; set; }
-    public int AccessTokenExpiration { get; set; }
-    public string SecurityKey { get; set; }
+    public string Audience { get; set; } // Token kullanıcı listesi
+    public string Issuer { get; set; } // Token imzalayan
+    public int AccessTokenExpiration { get; set; } // Dakika cinsinden süre
+    public string SecurityKey { get; set; } // Güvenlik anahtarı
 }
--- core/Core.Entity/Concrete/User.cs
+++ core/Core.Entity/Concrete/User.cs
@@ -14,3 +14,6 @@ public class User : BaseEntity
     public byte[] PasswordHash { get; set; }
     public string Status { get; set; }
 }
+
+
+
